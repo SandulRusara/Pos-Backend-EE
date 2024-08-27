3bd0d53070c3f43663736f9521a41e1896542f7d
SandulRusara <sandulrusara8@gmail.com>
2024-08-27 21:24:59 +0530

    Order update dao

---
+++ src/main/java/lk/ijse/posbackendee/dao/custom/OrderDAO.java
@@ -0,0 +1,7 @@
+package lk.ijse.posbackendee.dao.custom;
+
+import lk.ijse.posback.dao.CrudDAO;
+import lk.ijse.posback.entity.Order;
+
+public interface OrderDAO extends CrudDAO<Order, String> {
+}
