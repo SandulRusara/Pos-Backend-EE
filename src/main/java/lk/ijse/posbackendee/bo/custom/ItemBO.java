e4cfca167ba4a5a132b7ca364802161425894e01
SandulRusara <sandulrusara8@gmail.com>
2024-08-27 19:08:00 +0530

    item controller

---
+++ src/main/java/lk/ijse/posbackendee/bo/custom/ItemBO.java
@@ -0,0 +1,20 @@
+package lk.ijse.posbackendee.bo.custom;
+
+import lk.ijse.posback.bo.SuperBO;
+import lk.ijse.posback.dto.ItemDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ItemBO extends SuperBO {
+
+    boolean createItem(ItemDTO itemDTO, Connection connection) throws SQLException;
+
+    boolean updateItem(ItemDTO itemDTO, Connection connection) throws SQLException;
+
+    boolean deleteItem(String itemId, Connection connection) throws SQLException;
+
+    List<ItemDTO> getAllItems(Connection connection) throws SQLException;
+}
+
